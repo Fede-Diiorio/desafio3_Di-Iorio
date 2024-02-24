7ad8302e923f62801f4bf0583b7497dc5ce543e6
Federico Di Iorio <federico_metallica@hotmail.com>
2024-02-24 20:05:17 -0300

    Primeros pasos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # desafio3_Di-Iorio
+
---
+++ src/sysPc_DB.js
@@ -0,0 +1,105 @@
+const products = [
+    {
+        "id": 0,
+        "code": "abc123",
+        "name": "Fuente Sentey 700W",
+        "price": 42000,
+        "stock": 10,
+        "img": "./Fuente.webp",
+        "description": "Fuente sentey 700w hbp700-gs 80 plus bronze active pfc autofan 20+4x1 4+4pinesx1 satax6 molexx 2pci-e6+2x2",
+        "alt": "Imagen Fuente Sentey",
+        "category": "Fuente"
+    },
+    {
+        "id": 1,
+        "code": "abc124",
+        "name": "Memoria Ram 8gb",
+        "price": 13500,
+        "stock": 60,
+        "img": "./RAM.webp",
+        "description": "Memoria Ddr3 8gb 1600mhz 1.5v Desktop Mushkin Essentials Latencia 11-11-11-28",
+        "alt": "Imagen Memoria RAM",
+        "category": "Memoria"
+    },
+    {
+        "id": 2,
+        "code": "abc125",
+        "name": "Disco Rigido 2tb",
+        "price": 43500,
+        "stock": 10,
+        "img": "./HDD.webp",
+        "description": "Disco rigido 2tb 3.5 sata 3 seagate barracuda desktop escritorio",
+        "alt": "Imagen Disco Rigido HDD",
+        "category": "Almacenamiento"
+    },
+    {
+        "id": 3,
+        "code": "abc126",
+        "name": "Webcam Philips",
+        "price": 12500,
+        "stock": 5,
+        "img": "./webcam.webp",
+        "description": "Webcam philips p406b fhd 1080p usb 1920x1080 30fps angulo de vision 70 grados",
+        "alt": "Imagen Webcam Philips",
+        "category": "Periféricos"
+    },
+    {
+        "id": 4,
+        "code": "abc127",
+        "name": "Placa Madre Asrock",
+        "price": 160000,
+        "stock": 10,
+        "img": "./mother.webp",
+        "description": "Mother asrock z690 extreme hdmi displayport 4xddr4 1xps2 5xusb 1xusbtipoc 3xpciex16 1xpciex1 8xsata2xm21xm2 wifi rj45 7.1 1700 compatible generacion 12 y 13 - Ya actualizado",
+        "alt": "Imagen Placa Madre Asrock",
+        "category": "Placa Madre"
+    },
+    {
+        "id": 5,
+        "code": "abc128",
+        "name": "Procesador Intel I7",
+        "price": 165000,
+        "stock": 5,
+        "img": "./procesador.webp",
+        "description": "Procesador intel comet lake i7-10700 8 nucleos 16 hilos 2.9ghz a 4.8ghz socket1200 solo windows 10 64Bits. Decima generacion trae cooler trae video integrado",
+        "alt": "Imagen Procesador Intel I7",
+        "category": "Procesador"
+    },
+    {
+        "id": 6,
+        "code": "abc129",
+        "name": "Disco Solido Crucial 1tb",
+        "price": 32000,
+        "stock": 10,
+        "img": "./m21tb.webp",
+        "description": "Disco solido SSD 1TB M2 Cruscial P3 22x80mm interface PCIE NVME 3D NAND lectura 3500MB escritura 1900mb PCIE G3 1X4.",
+        "alt": "Imagen Disco M.2 Crucial",
+        "category": "Almacenamiento"
+    },
+    {
+        "id": 7,
+        "code": "abc130",
+        "name": "Disco Solido ADATA 120gb",
+        "price": 9700,
+        "stock": 30,
+        "img": "./ssd120.webp",
+        "description": "Disco solido SSD 120BG 2.5 sata III Adata ultimate SU650 lectura 520MB escritura 450MB.",
+        "alt": "Imagen Disco SSD ADATA",
+        "category": "Almacenamiento"
+    },
+    {
+        "id": 8,
+        "code": "abc131",
+        "name": "Disco Solido Kingston 240gb",
+        "price": 13000,
+        "stock": 20,
+        "img": "./ssd240.webp",
+        "description": "Disco solido SSD 240GB 2.5 sata III Kingston A400 lectura hasta 500MB y escritura hasta 450MB.",
+        "alt": "Disco SSD Kingston",
+        "category": "Almacenamiento"
+    }
+];
+
+module.exports = products;
+
+
