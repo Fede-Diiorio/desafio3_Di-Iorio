ba78e0eb69a0a07851b5c5f9c713095dd4221b1e
Federico Di Iorio <federico_metallica@hotmail.com>
2024-02-25 16:05:53 -0300

    Se realizó el ejercicio 1 de la clase 6

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo desde /');
+});
+
+app.get('/saludo', (req, res) => {
+    res.send('Hola mundo desde express');
+});
+
+app.listen(8080, () => {
+    console.log('Servidor Listo')
+});
