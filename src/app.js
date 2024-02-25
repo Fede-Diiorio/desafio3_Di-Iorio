349d2e20254aee35c8806f93e0f4b46f035c72f5
Federico Di Iorio <federico_metallica@hotmail.com>
2024-02-25 16:53:21 -0300

    Se agregó el query limit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,24 @@ const express = require('express');
 const app = express();
 
 const main = async () => {
-    console.log(await manager.getProducts());
+    const products = await manager.getProducts();
+    manager.addProduct('Placa De Video MSI GTX1630', 'Placa de video MSI GTX1630 Ventus 4gb gGDDR6 doble ventilador 1 displayPort 1 HDMI 1 DVI-D XS OC edition. No rquiere alimentación externa.', 200000, './gtx1630.webp', 'abc132', 5);
+
+    app.get('/products', (req, res) => {
+        const limitFilter = req.query.limit;
+
+        if (limitFilter) {
+            const limit = parseInt(limitFilter);
+            const limitedProducts = products.slice(0, limit);
+            res.json(limitedProducts);
+        } else {
+            res.json(products);
+        }
+    });
+
+    app.listen(3000, () => {
+        console.log('Server Listo!');
+    });
 }
 
 main();
