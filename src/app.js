1843886ded71848f7c2ac60283b193e6c587f381
Federico Di Iorio <federico_metallica@hotmail.com>
2024-02-26 18:12:48 -0300

    Se agregaró nuevo manejo de errores y se movió products.json

--- src/app.js
+++ src/app.js
@@ -4,11 +4,16 @@ const express = require('express');
 const app = express();
 
 const main = async () => {
-    const manager = new ProductManager('../products.json')
+    const manager = new ProductManager('./products.json')
     const products = await manager.getProducts();
 
     app.get('/products', (req, res) => {
         const limitFilter = req.query.limit;
+
+        if (limitFilter <= 0) {
+            res.status(400).json({ error: 'El límite debe ser superior a 0' });
+            return;
+        }
 
         if (limitFilter) {
             const limit = parseInt(limitFilter);
@@ -19,8 +24,8 @@ const main = async () => {
         }
     });
 
-    app.get('/products/:id', async (req, res) => {
-        const productId = parseInt(req.params.id);
+    app.get('/products/:pid', async (req, res) => {
+        const productId = parseInt(req.params.pid);
         const product = await manager.getProductById(productId);
         product ? res.json(product) : res.json('El producto no existe');
     })
